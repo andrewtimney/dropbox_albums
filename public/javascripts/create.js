d357bd34af266e51f0e48b80139eac3f3f08c7a2
Andrew Timney <andrew.timney@city-holdings.co.uk>
2015-09-25 17:26:00 +0100

    moved create markup

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -1,32 +1,25 @@
-	var results;
-	
-	var button = Dropbox.createChooseButton({
-		linkType: "direct",
-		multiselect: true,
-		extensions: ['images'],
-		success: function(files){
-			console.log(files);
-			results = files;
-			var selected = document.getElementById("selected");
-			selected.innerHTML = '';
-			files.forEach(function(file){
-				var img = document.createElement('img');
-				img.src = parseUrl(file.thumbnailLink);
-				selected.appendChild(img);					
-			});
-			
-			var resultsEl = document.getElementById("results");
-			resultsEl.value = JSON.stringify(files);
-		}
-	});
-	
-	document.getElementById("container").appendChild(button);	
-	
-	// document.querySelector('#create').addEventListener('click', function(){
-	// 	
-	// }, false);
-			
-			
-	function parseUrl(url){
-		return url.replace(/(mode=)[^\&]+/, 'mode=crop');
+var button = Dropbox.createChooseButton({
+	linkType: "direct",
+	multiselect: true,
+	extensions: ['images'],
+	success: function(files){
+		
+		console.log(files);
+		var selected = document.getElementById("selected");
+		selected.innerHTML = '';
+		files.forEach(function(file){
+			var img = document.createElement('img');
+			img.src = setMode(file.thumbnailLink);
+			selected.appendChild(img);					
+		});
+		
+		var resultsEl = document.getElementById("results");
+		resultsEl.value = JSON.stringify(files);
 	}
+});
+
+document.getElementById("container").appendChild(button);	
+		
+function setMode(url){
+	return url.replace(/(mode=)[^\&]+/, 'mode=crop');
+}

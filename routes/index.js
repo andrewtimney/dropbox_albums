89ebff7ecf69eb0a2fe80d9e64de77678746c5b2
Andrew Timney <Andrew.Timney@city-holdings.co.uk>
2015-09-25 16:52:02 +0100

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var shortid = require('shortid');
+var path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+  
+  var files = JSON.parse(req.body.results);
+  
+  var album = {
+    files: files,
+    expires: new Date().setDate(new Date().getDate()+1),
+    id: shortid.generate()
+  };
+  console.log(album.id);
+    
+  fs.writeFile(
+    path.join(__dirname, '../published', album.id+'.json'), 
+    JSON.stringify(album), 
+    function(err){
+      if(err) throw err;    
+    });
+  
+  res.render('index', { title: 'Express', body: album.id });
+});
+
+router.get('/o', function(req, res, next) {
+  
+  var filePath = path.join(__dirname, '../published', req.query.i+'.json');
+  console.log(filePath);
+  
+  var file = fs.readFileSync(filePath, 'utf8');
+  var album = JSON.parse(file);
+  
+  res.render('album', {album: album});
+});
+
+module.exports = router;

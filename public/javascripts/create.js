6d1e9ba426f0e6e84ceee3a45c89cbe96428b198
Andrew Timney <andrew.timney@city-holdings.co.uk>
2015-09-28 14:21:43 +0100

    Created own dropbox button

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -1,25 +1,31 @@
-var button = Dropbox.createChooseButton({
-	linkType: "direct",
-	multiselect: true,
-	extensions: ['images'],
-	success: function(files){
-		
-		console.log(files);
-		var selected = document.getElementById("selected");
-		selected.innerHTML = '';
-		files.forEach(function(file){
-			var img = document.createElement('img');
-			img.src = setMode(file.thumbnailLink);
-			selected.appendChild(img);					
-		});
-		
-		var resultsEl = document.getElementById("results");
-		resultsEl.value = JSON.stringify(files);
-	}
+document.querySelector('#select').addEventListener('click',function(){
+	
+	Dropbox.choose({
+		linkType: "direct",
+		multiselect: true,
+		extensions: ['images'],
+		success: function(files){
+			
+			console.log(files);
+			var selected = document.getElementById("selected");
+			selected.innerHTML = '';
+			files.forEach(function(file){
+				var img = document.createElement('img');
+				img.src = setBoundingBox(setMode(file.thumbnailLink));
+				selected.appendChild(img);					
+			});
+			
+			var resultsEl = document.getElementById("results");
+			resultsEl.value = JSON.stringify(files);
+		}
+	});
+
 });
 
-document.getElementById("container").appendChild(button);	
-		
 function setMode(url){
 	return url.replace(/(mode=)[^\&]+/, 'mode=crop');
+}
+
+function setBoundingBox(url){
+	return url.replace(/(bounding_box=)[^\&]+/, 'bounding_box=256');
 }

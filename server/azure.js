3b6345d0448acb1b71ba9d40758a1e528dc6fafd
Andrew Timney <andrew.timney@city-holdings.co.uk>
2015-10-01 10:14:54 +0100

    For azure

--- server/azure.js
+++ server/azure.js
@@ -2,33 +2,35 @@ var azureStorage = require('azure-storage');
 var config = require('../azureconfig');
 var path = require('path');
 
-var IMG_CONTAINER_NAME = "images";
-var ALBUM_CONTAINER_NAME = "albums";
-var blobService = azureStorage.createBlobService('UseDevelopmentStorage=true;');//config.accountName, config.accountKey);
-
-blobService.createContainerIfNotExists(IMG_CONTAINER_NAME, {
-  publicAccessLevel: 'blob'
-}, 
-function(error, result, response){
-	if(error) console.log(error);	
-});
-
-blobService.createContainerIfNotExists(ALBUM_CONTAINER_NAME, {
-  publicAccessLevel: 'blob'
-}, 
-function(error, result, response){
-	if(error) console.log(error);	
-});
-
-function uploadImage(file){
-	blobService.createBlockBlobFromLocalFile(
-		IMG_CONTAINER_NAME, path.basename(file), file, 
-		function(error, result, response) {
-			if (!error) {
-				// file uploaded
-				console.log(`File uploaded: ${file}`);
-			}
-		});
+if(!process.env.IsAzure){
+	
+	var IMG_CONTAINER_NAME = "images";
+	var ALBUM_CONTAINER_NAME = "albums";
+	var blobService = azureStorage.createBlobService('UseDevelopmentStorage=true;');//config.accountName, config.accountKey);
+	
+	blobService.createContainerIfNotExists(IMG_CONTAINER_NAME, {
+	publicAccessLevel: 'blob'
+	}, 
+	function(error, result, response){
+		if(error) console.log(error);	
+	});
+	
+	blobService.createContainerIfNotExists(ALBUM_CONTAINER_NAME, {
+	publicAccessLevel: 'blob'
+	}, 
+	function(error, result, response){
+		if(error) console.log(error);	
+	});
+	
+	function uploadImage(file){
+		blobService.createBlockBlobFromLocalFile(
+			IMG_CONTAINER_NAME, path.basename(file), file, 
+			function(error, result, response) {
+				if (!error) {
+					// file uploaded
+					console.log(`File uploaded: ${file}`);
+				}
+			});
+	}
 }
-
 module.exports = {};

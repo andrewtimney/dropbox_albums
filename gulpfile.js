a6de391b2ce138f015dbc3770a735df0636c20ff
Andrew Timney <andrew.timney@city-holdings.co.uk>
2015-10-01 11:27:03 +0100

    Added livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require("gulp");
+var server = require('gulp-express');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('server', function(){
+  
+  server.run(['bin/www']);
+  
+  gulp.watch(['public/**/*.js', 'public/**/*.css', 'server/views/**/*.vash'], function(event){
+      server.notify(event)
+      console.log('something changed', arguments);
+  });
+  
+  gulp.watch(['bin/www', 'app.js', 'server/routes/**/*.js'], [server.run]);
 });

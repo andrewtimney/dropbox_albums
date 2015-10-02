c382ea334c89e194d331ee46c8a129f05d4a8155
Andrew Timney <andrew.timney@city-holdings.co.uk>
2015-10-02 17:19:43 +0100

    Added react

--- public/javascripts/create.jsx
+++ public/javascripts/create.jsx
@@ -29,3 +29,46 @@ function setMode(url){
 function setBoundingBox(url){
 	return url.replace(/(bounding_box=)[^\&]+/, 'bounding_box=256');
 }
+
+var Images = React.createClass({
+	render: function(){
+		var images = [];
+		for(var i = 0; i < this.props.images.length; i++){
+			images.push(<img src={this.props.images[i].thumbnailLink} />);
+		}
+		return <div>{images}</div> 
+	}
+});
+
+var DropboxButton = React.createClass({
+	success: function(){
+	},
+	handleClick: function(){
+		
+		Dropbox.choose({
+			linkType: "direct",
+			multiselect: true,
+			extensions: ['images'],
+			success: this.success
+		});
+	},
+	render: function(){
+		return <button onClick={this.handleClick}>
+				 Select Dropbox Pictures
+			   </button>;
+	}
+});
+
+var Form = React.createClass({
+	render: function(){
+		return <div>
+				<h2>Album</h2>
+				<DropboxButton />
+			</div>;
+	}
+});
+
+React.render(
+	<Form />,
+	document.getElementById('react')
+);

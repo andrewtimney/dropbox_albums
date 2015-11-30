d537e1753e5e9ad5cd824dbae3be76f5d00df874
Andrew Timney <andrew.timney@gmail.com>
2015-11-30 22:11:21 +0000

    Layout using bootstrap

--- components/create.jsx
+++ components/create.jsx
@@ -1,4 +1,7 @@
 var Button = ReactBootstrap.Button;
+var Grid = ReactBootstrap.Grid;
+var Row = ReactBootstrap.Row;
+var Col = ReactBootstrap.Col;
 
 var Images = React.createClass({
 	setMode(url){
@@ -12,12 +15,15 @@ var Images = React.createClass({
 	},
 	render(){
 		var images = [];
+		var line = this.props.images.length > 0 ? <hr /> : '';
 		for(var i = 0; i < this.props.images.length; i++){
 			images.push(
-				<img src={this.setThumbnailProps(this.props.images[i].thumbnailLink)} />
+				<img src={this.setThumbnailProps(this.props.images[i].thumbnailLink)} className="image" />
 			);
 		}
+
 		return <div id="selected">
+			{line}
 			{images}
 		</div>;
 	}
@@ -33,13 +39,13 @@ var DropboxButton = React.createClass({
 		});
 	},
 	render: function(){
-		return <div>
-				<Button
+		return <span>
+		   <Button
 				 onClick={this.handleClick}
 				 bsStyle="primary">
 				 Select Dropbox Pictures
-			    </Button>
-			  </div>;
+			 </Button>
+		 </span>;
 	}
 });
 
@@ -55,7 +61,8 @@ var Form = React.createClass({
 		return <div>
 				<DropboxButton files={this.state.files} onSuccess={this.gotFiles} />
 				<form method="POST">
-					<input type="email" name="email" placeholder="Email Address" required />
+					<input type="email" name="email" placeholder="Email Address" required
+						className="form-control" />
 					<Images images={this.state.files} />
 					<input type="hidden" id="results" name="results" value={JSON.stringify(this.state.files)} />
 					<button type="submit" id="create">Create</button>
@@ -64,7 +71,42 @@ var Form = React.createClass({
 	}
 });
 
+var Create = React.createClass({
+	getInitialState(){
+		return { files: []};
+	},
+	gotFiles(files){
+		this.setState({files:files});
+	},
+	render(){
+	 return	<Grid>
+						<Row>
+							<Col md={12}>
+								<h1>Share your dropbox pictures</h1>
+								<hr/>
+							</Col>
+						</Row>
+						<Row>
+							<Col md={4}>
+								<DropboxButton files={this.state.files} onSuccess={this.gotFiles} />
+							</Col>
+							<Col md={4}>
+								<input type="email" name="email" placeholder="Email Address" />
+								<Button bsStyle="default">
+									Create Album
+								</Button>
+							</Col>
+						</Row>
+						<Row>
+							<Col md={12}>
+								<Images images={this.state.files} />
+							</Col>
+						</Row>
+					</Grid>;
+	}
+});
+
 React.render(
-	<Form />,
+	<Create />,
 	document.getElementById('react')
 );

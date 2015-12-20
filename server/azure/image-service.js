4e373b84983eb9b1e11ced6e4a14b172b00c3747
Andrew Timney <andrew.timney@gmail.com>
2015-12-20 23:05:22 +0000

    Cannot delete files wtf

--- server/azure/image-service.js
+++ server/azure/image-service.js
@@ -3,14 +3,15 @@ var utils = require('../utils')
 var Promise = require('bluebird');
 var fs = require('fs');
 var path = require('path');
+var fse = require('fs-extra');
 
 function downloadImage(url, folderName){
 	return new Promise(function(resolve, reject){
 		try{
 				var tempFolder = utils.getTempFolder(folderName);
 				var tempPath = utils.getTempPath(tempFolder, url);
-
 				var fileStream = fs.createWriteStream(tempPath);
+
 				blob.download(url, tempPath)
 				.then(function(response){
 					 var stream = response.pipe(fileStream);
@@ -30,6 +31,7 @@ function uploadImage(file, album){
 	return blob.upload(file, album.id)
 		.then(function(result){
 			album.azureFiles.push(decodeURI(result.replace('\\', '/')));
+			//fse.removeSync(file);
 		});
 }
 
@@ -45,10 +47,27 @@ function uploadImages(album){
 		);
 	}
 	return Promise.all(promises).then(function(){
+		var tempFolder = path.join(__dirname, '../../temp', album.id);
+		//console.log(tempFolder);
+		//deleteFolderRecursive(tempFolder);
 		return album;
 	});
 }
 
 module.exports = {
 	uploadImages: uploadImages
+};
+
+var deleteFolderRecursive = function(path) {
+  if( fs.existsSync(path) ) {
+      fs.readdirSync(path).forEach(function(file) {
+        var curPath = path + "/" + file;
+          if(fs.statSync(curPath).isDirectory()) { // recurse
+              deleteFolderRecursive(curPath);
+          } else { // delete file
+              fs.unlinkSync(curPath);
+          }
+      });
+      fs.rmdirSync(path);
+    }
 };
